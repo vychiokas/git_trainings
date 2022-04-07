762dddc602c2ecb0d5e01e1939ac70c7e65025b7
Vytautas Sluckas <vytautas.sluckas@exacaster.com>
2022-04-07 19:50:38 +0300

    adding calculator bones

---
+++ calculator/calculator.py
@@ -0,0 +1,15 @@
+def add(arg1, arg2):
+  #   Your code goes here
+  return((arg1 + arg2) / 2)
+
+def subtract(arg1, arg2):
+  #   Your code goes here
+  return(arg1 - arg2)
+
+def multiply(arg1, arg2):
+  #   Your code goes here
+  return(arg1 * arg2)
+
+def divide(arg1, arg2):
+  #   Your code goes here
+  return(arg1 / arg2)
